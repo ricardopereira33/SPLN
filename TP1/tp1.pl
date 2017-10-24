16fd721e69a2a15c97ce5e89bb4e9f7cca49fc3d
9thScientist <bruno.ms.cancelinha@gmail.com>
2017-10-24 17:24:19 +0100

    bota

--- TP1/tp1.pl
+++ TP1/tp1.pl
@@ -3,29 +3,34 @@
 our ($alpha);
 use strict;
 use utf8::all;
+
 $/ = ''; # processar paragrafo a paragrafo
 
+# var with the searched name
+#my $wanted = "Cosette";
+my $wanted = shift;
 
 # list with the neighbords names
 my %names;
+# lists failed names
+my %failed = ("Em", "Aquela", "Lá", "Isto", "Duas", "Chamava", "Verão", "Assim", "Bem");
 
 # regex to find the left and right side (the $center is for all names)
-my $left   = qr[(?<=\b(.{21}))];
+#my $left   = qr[(?<=\b(.{21}))];
+my $np= qr[(\{.*?\})];
+my $left   = qr[(([ \w{}]+){1,5})];
 my $center = qr[\{(.*?)\}];
-my $right  = qr[(?=(.{21}))];
-
-# var with the searched name
-my $wanted = "Cosette";
+my $right  = qr[(?=(( [\w{}]+){1,5}))];
 
 # var with the output file
 my $files = 'output.dot';
 my $OUTFILE;
 
 while (<>){
-    while(/$left\{$wanted\}$right/g){
+    while(/$np$left\{$wanted\}$right/g){
       print("-------------------------------------------------\n");
-      print("Left = '$1' \nCenter =  '$wanted'\nRight = '$2'\n");
-      print("Original Phrase: $1 $wanted $2\n");
+      print("Left = '$1 $2' \nCenter = '$wanted'\nRight = '$4'\n");
+      print("Original Phrase: $1 $2 $wanted $4\n");
       print("-------------------------------------------------\n");
       findNames($1);
       findNames($2);
@@ -55,13 +60,14 @@ sub findNames{
     my $words = shift;
     print("Phrase:\t$words\n\n");
 
-    if($words =~ /$center/g){
+    if($words =~ /$center/g && ){
         print("-> Find:\t$1\n\n");
         $names{$1}++;
     }
 
     return;
 }
+
 
 ## Man instructions
 
@@ -77,6 +83,9 @@ TP1
 =head1 DESCRIPTION
 
 Regista todos os nomes anotados numa vizinhança de k caracteres.
+
+=head2 DEPENDENCIES
+Chart::OFC2
 
 =head2 EXPORT
 
