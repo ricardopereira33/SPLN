b07b9614586df262445730dc8016088ce89e6507
9thScientist <bruno.ms.cancelinha@gmail.com>
2018-01-27 21:45:12 +0000

    Proverbial now prepares statements

--- TP2/theLab/proverbial.rb
+++ TP2/theLab/proverbial.rb
@@ -1,6 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
 
+Irrelevant = /\b(d[aeo]|[aeo]|com|[àao]+s|para|em|és?|uma?|[et]u|que)\b/
+
 def get_proverbs 
   html = open('http://natura.di.uminho.pt/~jj/pln/proverbio.dic')
   doc = Nokogiri::HTML( html.read )
@@ -13,7 +15,7 @@ end
 
 def get_keywords(phrase)
   tokens = phrase.split
-  tokens.delete_if { |tkn| tkn.match(/\b(d[aeo]|[aeo]|com|[àao]+s|para|em|és?|uma?|[et]u)\b/)}
+  tokens.delete_if { |tkn| tkn.match(Irrelevant)}
 end
 
 def relevant_proverb(phrase, proverbs)
@@ -27,12 +29,18 @@ def relevant_proverb(phrase, proverbs)
   return relevant
 end
 
+def prepare_statement(statement)
+  unless statement.nil?
+    statement.gsub(/\(([^\)\|]*)\|([^\)\|]*)(\|.*)?\)/, "\\#{rand(1..2)}")
+  end
+end
+
 if __FILE__ == $0
   proverbs = get_proverbs
 
   loop do
     phrase = gets.chomp
     ps = relevant_proverb(phrase, proverbs)
-    puts ps.sample
+    puts prepare_statement(ps.sample)
   end
 end
