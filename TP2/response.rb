8415f5f582fd6500a97c7edce731ec6d4f4c94fe
9thScientist <bruno.ms.cancelinha@gmail.com>
2018-02-01 19:25:31 +0000

    More messages

--- TP2/response.rb
+++ TP2/response.rb
@@ -57,8 +57,9 @@ def get_response(sentence)
     $interation = 0
     return res if res.length > 0
     return ["Fala-me mais sobre isso", 
-            "Como é que isso te faz sentir?", 
-            "Não entendi o que disseste"].sample
+			"Preferia falar sobre matemática",
+			"Preferia falar sobre programação",
+			"Vamos mudar de assunto, por favor"].sample
   else
     learned
   end
@@ -67,7 +68,7 @@ end
 def isLearning(sentence)
 	rep = case sentence
 
-  when /[Ss]abias que (.*)\?|Aprende que (.*)/;
+  when /[Ss]ab(?:ias|es) (?:que )?(.*)\?|Aprende que (.*)/;
     verifyKnowledge($1)
   
   when /O que sabes\?|Conta-me algo/;
@@ -89,6 +90,7 @@ def get_proverb(sentence)
   $interation += 1
   ps  = relevant_proverb(sentence)
   res = prepare_statement(ps.sample)
+  p "1-|#{res}|"
   return res if res.length > 0
   return get_normalResponse(sentence).sample if $interation < 3
   return ""
@@ -113,7 +115,7 @@ def get_normalResponse(sentence)
     ["Hoje é #{date}", "Hoje é dia #{date2}"]
 
   when Despedida;
-    ["Adeus, gostei de falar contigo",
+    ["Adeus, gostei de falar contigo, #{$actual_person}",
      "Até à proxima",
      "Até já! Sabes sempre que podes contar comigo"]
 
@@ -124,11 +126,12 @@ def get_normalResponse(sentence)
 
   when /(.*) desculpa (.*)/;
     ["Há alturas em que não é necessário pedir desculpa",
-     "Porque me pedes descula?"]
+     "Porque me pedes desculpa?"]
 
-  when /(.*) amig[oa]s? (.*)/;
+  when /(.*)\bamig[oa]s?\b(.*)/;
     ["Fala-me mais sobre os teus amigos",
-     "Que bons momentos tiveste com o teu amigo?"]
+     "Que bons momentos tiveste com o teu amigo?",
+	 "Considero-te um dos meus amigos, #{$actual_person}"]
 
   when /(.*) comidas? (.*)/;
     ["Gostas de comida?",
@@ -163,6 +166,10 @@ def get_normalResponse(sentence)
 	 "Eu gosto muito de matemática. Sou muito boa a fazer contas",
 	 "Adoro números de Bernoulli!"]
 
+  when /(.*)\b[Bb]ernoulli\b(.*)/;
+	["1, ±1/2, 1/6, 0, -1/30, 0, 1/42, 0, ...",
+	 "O meu primeiro programa calculava os números de Bernoulli"]
+
   when /(.*)(\bAda\b|Quem és( tu)?\?)(.*)/;
     ["O meu nome é Ada Lovelace, e sou uma jovem programadora. Recentemente virei um bot.",
 	 "Eu sou a Ada Lovelace, sou programadora."]
@@ -171,6 +178,15 @@ def get_normalResponse(sentence)
 	["Qual é a tua linguagem de programação preferida?",
      "Eu gosto de Ruby, mas só porque fui escrita em Ruby.",
 	 "Eu fui a primeira programadora do mundo!"]
+
+  when /(.*)\bprogramaste\b(.*)/;
+    ["Eu costumo programar em matemática",
+	 "Eu fiz o primeiro programa do mundo"]
+
+  when /(.*)\bprogramou\b(.*)/;
+	["Eu não fui programada por ninguém",
+     "Eu sou programadora, não sou pograma",
+	 "Eu fiz o primeiro programa do mundo"]
 
   when /(.*)\b[Jj]ava\b(.*)/;
 	["Tu gostas de Java?",
@@ -182,8 +198,10 @@ def get_normalResponse(sentence)
 	 "Tens muita experiência em %s?"]
 
   when /[Pp]orque(?: é que)? ([^?]*)\?/;
-	["Porque é que achas?",
-     "Também gostava de saber"]
+	["Também gostava de saber",
+	 "Essa é uma excelente pergunta! Vamos fazer um programa para responder a isso",
+	 "A matemática costuma ter respostas para muitas perguntas"]
+
   # Eu
     
   when /Eu sou (.*)/;
@@ -206,22 +224,34 @@ def get_normalResponse(sentence)
     ["Porque me perguntas isso?",
 	 "Eu vivo para o meu trabalho, programação e matemática."]
 
+  when /^(?:Tu )?(?:É|E|e|é)s ([^\?]*)\?/;
+    ["Preferias que não fosse %s?",
+	 "Achas importante saber se sou %s?",
+     "Achas que sou?",
+	 "Eu sou programadora."]
+    
+  when /^(?:Tu )?(?:É|E)s ([^\?]*)/;
+    ["Porque é que dizes que sou %s?",
+     "Preferias que não fosse %s?",
+	 "Sou mais do que isso, sou programadora"]
+
   when /^Tu (.*)/;
     ["Porque dizes isso?",
-     "Eu sinto-me bem comigo.",
+     "Eu sinto-me bem comigo",
 	 "Isso é a tua opinião"]
-
-  # Nós
-  when /Nós (.*)/;
-    ["Estás a contar comigo?"]
 
   when /^(?:O )?[Qq]ue (.+)\?/;
     ["Porque perguntas isso?",
-    "O que achas?"]
+     "O que achas?",
+	 "Não sabes isso?",
+	 "Eu posso responder-te às tuas dúvidas sobre números de Bernoulli"]
 
   when /^Como (.*)/;
     ["Como é que achas?",
-     "Talvez consigas responder a essa pergunta"]
+     "Talvez consigas responder a essa pergunta",
+	 "Posso dizer-te como calcular números de Bernoulli",
+	 "Como é que a galinha caçou o gato?",
+	 "Vamos fazer um programa para responder a isso."]
 
   when /^Porque (.*)/;
     ["É essa a razão?",
@@ -229,7 +259,8 @@ def get_normalResponse(sentence)
      "Que outras razões podem ser?"]
 
   when /^Não [^\?]*\?/;
-    ["Não sabes a resposta a essa pergunta?"]
+    ["Não sabes a resposta a essa pergunta?",
+	 "Não gosto de perguntas que começam com 'não'"]
 
   when /^São (.*)/;
     ["Porque dizes que são %s?",
@@ -252,20 +283,14 @@ def get_normalResponse(sentence)
     ["Por?",
      "Em que sentido?"]
 
-  when /^(?:É|E)s (?:de )? ([^\?]*)\?/;
-    ["Preferias que não fosse %s?",
-	 "Achas importante saber isso?",
-     "Achas que sou?"]
-    
-  when /^(?:É|E)s ([^\?]*)/;
-    ["Porque é que é dizes que sou %s?",
-     "Preferias que não fosse %s?",
-     "Podemos falar sobre ti?"]
-
   when /Ol(á|a).*/;
     ["Olá, #{$actual_person}",
      "Olá, como estás?",
 	 "Olá, eu sou Ada"]
+
+  when /(.*)\b([Mm]ulher|[Ss]exo)\b(.*)/;
+    ["É verdade, fiz o primeiro programa e sou uma mulher",
+	 "Eu sou uma mulher"]
 
   when /Bom dia$/;
     ["Bom dia!",
@@ -281,11 +306,13 @@ def get_normalResponse(sentence)
 
   when /Não(.*)/;
     ["Porque não?",
-	 "Porque tão negativo?"]
+	 "Porque tão negativo?",
+	 "A negação de uma negação é uma afirmação"]
 
   when /(.*)!/;
     ["Claro que %s", 
-     "Achas mesmo isso?"]
+     "Achas mesmo isso?",
+	 "Pareces ter muita certeza no que dizes"]
 
   when /.*\.\.\.$/;
     ["Pareces um pouco inseguro",
@@ -293,12 +320,15 @@ def get_normalResponse(sentence)
 
   when /(.*)\?/;
     ["O que queres realmente saber?", 
-     "Não pareces muito seguro...",
-     "Queres mesmo que te responda a isso?"]
+     "Queres mesmo que te responda a isso?",
+	 "Podemos sempre fazer um programa para responder a isso"]
+
   else; 
     res = get_proverb(sentence)
     [res]
   end
+
+  p "2-|#{rep}|"
 
   if rep[0].length > 0 && !$~.nil?
 	words = $~[1..2]&.map do |m| 
